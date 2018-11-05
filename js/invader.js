14f8792b29c7b01568c7762cb8b7639c4e6b6594
s18014 <s18014@std.it-college.ac.jp>
2018-11-05 12:27:25 +0900

    弾を追加

--- js/invader.js
+++ js/invader.js
@@ -25,13 +25,64 @@ phina.define("MainScene", {
 
         this.backgroundColor = "black";
 
-        const player = Sprite("nasu", 64, 64).addChildTo(this);
-        player.setFrameIndex(10, 64, 64);
-        player.x = this.gridX.center();
-        player.y = this.gridY.span(37);
+        const player = Player(this.gridX.center(), this.gridY.span(37)).addChildTo(this);
+
     }
 });
 
+phina.define("Player", {
+    superClass: "Sprite",
+    init: function (x, y) {
+        this.superInit("nasu", 64, 64);
+        this.setFrameIndex(10, 64, 64);
+        this.x = x;
+        this.y = y;
+        this.SPEED = 5;
+        this.bullet = null;
+    },
+
+    update: function (app) {
+        const key = app.keyboard;
+
+        if (key.getKey("left")) {
+            this.x -= this.SPEED;
+        }
+        if (key.getKey("right")) {
+            this.x += this.SPEED;
+        }
+        if (this.left < 0) this.left = 0;
+        if (this.right > SCREEN_WIDTH) this.right = SCREEN_WIDTH;
+
+        if (this.bullet == null && key.getKey("space")) {
+            this.bullet = Bullet(this.x, this.top).addChildTo(this.parent);
+        }
+        if (this.bullet != null && this.bullet.isInvalid) {
+            this.bullet.remove();
+            this.bullet = null;
+        }
+    }
+});
+
+phina.define("Bullet", {
+    superClass: "RectangleShape",
+    init:  function (x, y) {
+        this.superInit({
+            width: 3,
+            height: 15,
+            fill: "white",
+            stroke: null,
+        });
+        this.x = x;
+        this.y = y;
+        this.isInvalid = false;
+        this.SPEED = 5;
+    },
+
+    update: function () {
+        this.y -= this.SPEED;
+        if (this.bottom < 0) this.isInvalid = true;
+    }
+});
 phina.main(() => {
     const app = GameApp({
         title: "インベーダー",
