16a5d4fa1db7082224295d20d6f7c39e4f88f335
s18014 <s18014@std.it-college.ac.jp>
2018-11-12 12:27:03 +0900

    enemy, enemyGroupクラスを追加 enemyGroupで敵全体を移動する処理を途中まで

--- js/invader.js
+++ js/invader.js
@@ -13,6 +13,10 @@ const ASSETS = {
     }
 };
 
+const ENEMY_ASSETS = [
+    "buro", "mero", "mika", "nasu", "take", "toma"
+];
+
 phina.define("MainScene", {
     superClass: "DisplayScene",
     init: function () {
@@ -22,11 +26,30 @@ phina.define("MainScene", {
         });
         this.gridX = Grid(SCREEN_WIDTH, 40);
         this.gridY = Grid(SCREEN_HEIGHT, 40);
-
         this.backgroundColor = "black";
+        this.time = 0;
+        this.enemyInterval = 2000;
 
-        const player = Player(this.gridX.center(), this.gridY.span(37)).addChildTo(this);
+        this.player = Player(this.gridX.center(), this.gridY.span(37)).addChildTo(this);
+        this.enemyGroup = EnemyGroup().addChildTo(this);
+        for (x=4; x<16; x++) {
+            for (y=1; y<8; y++) {
+                this.enemyGroup.addChild(Enemy(this.gridX.span(x * 2), this.gridY.span(y * 3), ENEMY_ASSETS[(x + y) % 5]));
+            }
+        }
+    },
+    update: function (app) {
+        if (this.player.bullet != null) {
+            this.enemyGroup.children.some(enemy => {
+                if (enemy.hitTestElement(this.player.bullet)) {
+                    this.player.bullet.flare('hit');
+                    enemy.flare('hit');
+                    return true;
+                }
 
+                return false;
+            });
+        }
     }
 });
 
@@ -57,11 +80,24 @@ phina.define("Player", {
             this.bullet = Bullet(this.x, this.top).addChildTo(this.parent);
         }
         if (this.bullet != null && this.bullet.isInvalid) {
-            this.bullet.remove();
             this.bullet = null;
         }
     }
 });
+
+phina.define("Enemy", {
+    superClass: "Sprite",
+    init: function (x, y, image) {
+        this.superInit(image, 64, 64);
+        this.setFrameIndex(7, 64, 64);
+        this.x = x;
+        this.y = y;
+    },
+    onhit: function () {
+        this.remove();
+    }
+});
+
 
 phina.define("Bullet", {
     superClass: "RectangleShape",
@@ -75,13 +111,50 @@ phina.define("Bullet", {
         this.x = x;
         this.y = y;
         this.isInvalid = false;
-        this.SPEED = 5;
+        this.SPEED = 50;
+    },
+
+    onhit: function () {
+        this.remove();
+        this.isInvalid = true;
     },
 
     update: function () {
         this.y -= this.SPEED;
-        if (this.bottom < 0) this.isInvalid = true;
+        if (this.bottom < 0) this.flare("hit");
     }
+});
+
+phina.define("EnemyGroup", {
+    superClass: "DisplayElement",
+    init: function () {
+        this.superInit();
+        this.time = 0;
+        this.interval = 200;
+        this.direction = 1;
+    },
+
+    update: function (app) {
+        this.time += app.deltaTime;
+        const scene = this.parent;
+        let right = 0;
+        let left = scene.gridX.columns;
+
+        if (this.time / this.interval >= 1) {
+            this.children.forEach(enemy => {
+                enemy.moveBy(scene.gridX.unit() * this.direction, 0);
+                right = Math.max(right, enemy.x / scene.gridX.unit());
+                left = Math.min(left, enemy.x / scene.gridX.unit());
+            });
+
+            this.time -= this.interval;
+        }
+
+        if (this.direction > 0 && right >= 38
+            || this.direction < 0 && left <= 2) {
+            this.direction = -this.direction;
+        }
+    },
 });
 phina.main(() => {
     const app = GameApp({
